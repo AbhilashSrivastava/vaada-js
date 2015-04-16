d73ae217ca07424da785b754f99bf095efdbb3f7
Ashmeet Sidhu <ashmeet.sidhu7@gmail.com>
2015-04-16 13:57:07 +0530

    problem statement

--- src/vaada.js
+++ src/vaada.js
@@ -5,5 +5,40 @@
 function Vaada() {
 }
 
+////////////
+// Case 1 //
+////////////
+var pr = new Vaada(function(resolve, reject) {
+  console.log('sending request')
+  console.log('response')
+  resolve('yes')
+})
+
+setTimeout(function() {
+  console.log('calling then')
+  pr.then(function(data) {
+    console.log(data);
+  }, function (error) {
+    console.log(error);
+  })
+}, 1000)
+
+////////////
+// Case 2 //
+////////////
+var pr = new Vaada(function(resolve, reject) {
+  console.log('sending request')
+  setTimeout(function() {
+    console.log('response')
+    resolve('yes')
+  }, 1000)
+})
+
+console.log('calling then')
+pr.then(function(data) {
+  console.log(data);
+}, function (error) {
+  console.log(error);
+})
 
 module.exports = Vaada;
